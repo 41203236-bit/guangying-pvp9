effd62be6eb05fc304cb3eb9ba012d5ca9b2fbb6
41203236-bit <41203236@gcloud.csu.edu.tw>
2026-04-03 20:15:37 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmjWi-07_edTrrCKGBjo1JHvz-o1CloPM",
+  authDomain: "guangying-pvp.firebaseapp.com",
+  databaseURL: "https://guangying-pvp-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "guangying-pvp",
+  storageBucket: "guangying-pvp.firebasestorage.app",
+  messagingSenderId: "529611021363",
+  appId: "1:529611021363:web:db39a8dd86d6d2f715d746"
+};
